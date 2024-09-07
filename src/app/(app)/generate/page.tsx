41117bd396e93aa33554434cb0e31726ae7b607a
myuta <myuta.dev@gmail.com>
2024-09-07 21:30:06 +0700

    change fetch url for languages

--- src/app/(app)/generate/page.tsx
+++ src/app/(app)/generate/page.tsx
@@ -2,16 +2,18 @@ import { Language } from '@/types/type'
 import GeneratePhraseForm from '@/components/GeneratePhraseForm'
 
 const getAllLanguageData = async () => {
-    const res = await fetch('http://localhost:8000/api/languages', {
-        cache: 'default',
-    })
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/languages`,
+        {
+            cache: 'default',
+        },
+    )
 
     const languagesData = await res.json()
     return languagesData
 }
 
 const Generate = async () => {
-
     const languagesData: Language[] = await getAllLanguageData()
     console.log(languagesData)
 
@@ -19,7 +21,7 @@ const Generate = async () => {
         <div className="mx-5 my-5">
             <div className="font-semibold text-xl">Ask AI for the phrase!</div>
             <div className="pt-5">
-                <GeneratePhraseForm languagesData={languagesData}  />
+                <GeneratePhraseForm languagesData={languagesData} />
             </div>
         </div>
     )
